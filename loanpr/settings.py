f9191f643883a45ca6748d0ad6c6f8cbcc6ed821
PranavPookuth <pranavpookuth@gmail.com>
2025-03-04 17:20:05 +0530

    Resolved merge conflicts

--- loanpr/settings.py
+++ loanpr/settings.py
@@ -150,8 +150,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+
 }
-z
+
 from datetime import timedelta
 
 SIMPLE_JWT = {
